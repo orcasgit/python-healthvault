a05eba84a9c65161cfb5f6408058307d7b9694a9
Dan Poirier <poirier@pobox.com>
2012-11-06 12:00:59 -0500

    Require pycrypto

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='python-healthvault',
     version='0.0.1',
-    packages=['healthvaultlib', 'healthvaultlib.Crypto', 'healthvaultlib.Crypto.PublicKey',
-              'healthvaultlib.Crypto.Util', 'webapp'],
+    packages=['healthvaultlib'],
     url='https://github.com/caktus/python-healthvault',
     license='Apache 2.0',
     author='Dan Poirier and Caktus Group',
     author_email='dpoirier@caktusgroup.com',
-    description='Python library to access Microsoft Healthvault'
+    description='Python library to access Microsoft Healthvault',
+    requires=['pycrypto>=2.6']
 )
