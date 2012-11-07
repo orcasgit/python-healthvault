3c8bd345780744943d11e0e9d4f8f3ceab88cd8f
Dan Poirier <poirier@pobox.com>
2012-11-07 11:50:37 -0500

    Get basic 'setup.py sdist' working

--- setup.py
+++ setup.py
@@ -9,5 +9,13 @@ setup(
     author='Dan Poirier and Caktus Group',
     author_email='dpoirier@caktusgroup.com',
     description='Python library to access Microsoft Healthvault',
-    requires=['pycrypto>=2.6', 'sphinx']
+    requires=['pycrypto (>=2.6)', 'sphinx'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
 )
