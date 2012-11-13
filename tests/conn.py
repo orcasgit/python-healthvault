7d12cc02dd38bd69a87f33b28b2eb1282726383f
Dan Poirier <poirier@pobox.com>
2012-11-13 11:43:57 -0500

    First very simple test

--- tests/conn.py
+++ tests/conn.py
@@ -2,7 +2,12 @@
 
 import mock
 from unittest import TestCase
+from healthvaultlib.healthvault import HealthVaultConn
 
 class ConnTests(TestCase):
-    def test_one(self):
-        print "PASSED!"
+    def test_very_simple(self):
+        # construct a conn, mocking all the real work
+        with mock.patch.object(HealthVaultConn, '_get_auth_token'):
+            with mock.patch.object(HealthVaultConn, '_get_record_id'):
+                c = HealthVaultConn()
+
