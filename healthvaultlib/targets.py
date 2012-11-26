40707d77b91aa559689ab577e264ea1fb96fdf7d
Rebecca Lovewell <rebecca@caktusgroup.com>
2012-11-26 09:37:16 -0500

    Used underscores in names

--- healthvaultlib/targets.py
+++ healthvaultlib/targets.py
@@ -11,26 +11,27 @@ class ApplicationTarget(object):
     For more information, see the `HealthVault documentation on application
     targets <http://msdn.microsoft.com/en-us/library/ff803620.aspx#returnApplicationTargets>`_.
     """
-    APPAUTHINVALIDRECORD = 'AppAuthInvalidRecord'
-    APPAUTHREJECT = 'AppAuthReject'
-    APPAUTHSUCCESS = 'AppAuthSuccess'
-    EDITRECORDCOMPLETE = 'EditRecordComplete'
-    EDITRECORDCANCEL = 'EditRecordCancel'
-    EDITRECORDCANCELED = 'EditRecordCanceled'
-    RECONCILECANCELED = 'ReconcileCanceled'
-    RECONCILECOMPLETE = 'ReconcileComplete'
-    RECONCILEFAILURE = 'ReconcileFailure'
-    SELECTEDRECORDCHANGED = 'SelectedRecordChanged'
-    SHARERECORDFAILED = 'ShareRecordFailed'
-    SHARERECORDSUCCESS = 'ShareRecordSuccess'
-    SIGNOUT = 'SignOut'
+    APP_AUTH_INVALID_RECORD = 'AppAuthInvalidRecord'
+    APP_AUTH_REJECT = 'AppAuthReject'
+    APP_AUTH_SUCCESS = 'AppAuthSuccess'
+    EDIT_RECORD_COMPLETE = 'EditRecordComplete'
+    EDIT_RECORD_CANCEL = 'EditRecordCancel'
+    EDIT_RECORD_CANCELED = 'EditRecordCanceled'
+    RECONCILE_CANCELED = 'ReconcileCanceled'
+    RECONCILE_COMPLETE = 'ReconcileComplete'
+    RECONCILE_FAILURE = 'ReconcileFailure'
+    SELECTED_RECORD_CHANGED = 'SelectedRecordChanged'
+    SHARE_RECORD_FAILED = 'ShareRecordFailed'
+    SHARE_RECORD_SUCCESS = 'ShareRecordSuccess'
+    SIGN_OUT = 'SignOut'
 
     @classmethod
     def all_targets(cls):
         """Returns a list of all possible application targets."""
-        return [cls.APPAUTHINVALIDRECORD, cls.APPAUTHREJECT,
-                cls.APPAUTHSUCCESS, cls.EDITRECORDCOMPLETE,
-                cls.EDITRECORDCANCEL, cls.EDITRECORDCANCELED,
-                cls.RECONCILECANCELED, cls.RECONCILECOMPLETE,
-                cls.RECONCILEFAILURE, cls.SELECTEDRECORDCHANGED,
-                cls.SHARERECORDFAILED, cls.SHARERECORDSUCCESS, cls.SIGNOUT]
+        return [cls.APP_AUTH_INVALID_RECORD, cls.APP_AUTH_REJECT,
+                cls.APP_AUTH_SUCCESS, cls.EDIT_RECORD_COMPLETE,
+                cls.EDIT_RECORD_CANCEL, cls.EDIT_RECORD_CANCELED,
+                cls.RECONCILE_CANCELED, cls.RECONCILE_COMPLETE,
+                cls.RECONCILE_FAILURE, cls.SELECTED_RECORD_CHANGED,
+                cls.SHARE_RECORD_FAILED, cls.SHARE_RECORD_SUCCESS,
+                cls.SIGN_OUT]
